f7ead75314b06212ecc2a93722f0707125601a10
Mauricio Cabrera <mcabrera630@gmail.com>
2020-10-29 19:13:19 -0400

    tightly Coupled

---
+++ Spring-00-TightlyCoupled/src/FullTimeMentor.java
@@ -0,0 +1,7 @@
+public class FullTimeMentor {
+
+    public void createAccount(){
+        System.out.println("Full Time Mentor Account is created..");
+
+    }
+}
---
+++ Spring-00-TightlyCoupled/src/Mentor.java
@@ -0,0 +1,13 @@
+public class Mentor {
+    FullTimeMentor fullTimeMentor;
+    PartTimeMentor partTimeMentor;
+
+    public Mentor(FullTimeMentor fullTimeMentor, PartTimeMentor partTimeMentor) {
+        this.fullTimeMentor = fullTimeMentor;
+        this.partTimeMentor = partTimeMentor;
+    }
+    public void manageAccount(){
+       fullTimeMentor.createAccount();
+       partTimeMentor.createAccount();
+    }
+}
